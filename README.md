b0f19fda7abd8c0c627908417a0f59ffb2565c87
Melissa <mel.chavez.code@gmail.com>
2022-11-25 18:30:58 -0500

    Add logic to add and get data to/from indexedDB

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # PWA Text Editor
 
 This is a a text editor that runs in the browser. It uses methods for getting and storing data to an IndexedDB database.
+
+## Contributions
+
+- MDN, dev.to
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,58 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("textEditor", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("textEditor")) {
+        console.log("textEditor database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("textEditor", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+      console.log("textEditor database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Added logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  // Open database with name = textEditor, version = 1
+  const db = await openDB("textEditor", 1);
+  const transaction = db.transaction(["textEditor"], "readwrite");
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const objStore = transaction.objectStore("content");
+  const request = objStore.add({ key: "content", value: content });
+
+  // if request successful
+  transaction.oncomplete = function (event) {
+    console.log(`data added to indexedDB: ${request.result}`);
+  };
+  // if error
+  transaction.onerror = function (event) {
+    console.log("Error: data not added to indexedDB");
+  };
+};
+
+// Added logic for a method that gets all the content from the database
+export const getDb = async () => {
+  // Open database with name = textEditor, version = 1
+  const db = await openDB("textEditor", 1);
+  const transaction = db.transaction(["textEditor"], "readonly");
+
+  const objStore = transaction.objectStore("content");
+  const request = objStore.get("content");
+
+  // if error
+  request.onerror = (event) => {
+    console.log(`Error: data not retrieved from indexedDB`);
+  };
+  // if request successful
+  request.onsuccess = (event) => {
+    console.log(`data retrieved from indexedDB: ${request.result}`);
+  };
+
+  return request.value;
+};
 
 initdb();
